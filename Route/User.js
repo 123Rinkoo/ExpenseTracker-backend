2e9039b4ba9f376a342b5699bcffe33d3765c8f0
rinkoo <rinkookandpal1966@gmail.com>
2022-12-05 14:11:06 +0530

    added bcrypt for securing passwords

--- Route/User.js
+++ Route/User.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../model/user')
+const bcrypt = require('bcrypt');
 
 router.post("/SigningUp", (req, res, next) => {
     // console.log(req.body.key1, req.body.key2, req.body.key3);  
+
     User.findAll()
         .then(users => {
             // console.log(users[0].email)
@@ -12,18 +14,21 @@ router.post("/SigningUp", (req, res, next) => {
                 return element.email == req.body.key2;
             });
             if (!found) {
-                User.create({ name: req.body.key1, email: req.body.key2, password: req.body.key3 });
-                
+                const saltRounds = 10;
+                bcrypt.hash(req.body.key3, saltRounds, function (err, hash) {
+                    User.create({ name: req.body.key1, email: req.body.key2, password: hash });
+                    return res.status(200).json({ success: true, message: "User SignUp sucessful" });
+                });
             }
-            else{
+            else {
                 // console.log('user exist');
-                return res.status(500).json({success: false, message:"User already exists"})
+                return res.status(500).json({ success: false, message: "User already exists" })
             }
         })
         .catch(err => console.log(err));
 })
 
-router.post('/login',(req, res, next)=>{
+router.post('/login', (req, res, next) => {
     // console.log(req.body.key1, req.body.key2);
     User.findAll()
         .then(users => {
@@ -33,14 +38,23 @@ router.post('/login',(req, res, next)=>{
                 return element.email == req.body.key1;
             });
             if (!found) {
-               return res.status(404).json({success: false, message:"User doesnot exist"});  
+                return res.status(404).json({ success: false, message: "User doesnot exist" });
             }
-            else if(found.password!== req.body.key2){
-                return res.status(401).json({success: false, message:"Password is wrong"}); 
+            else {
+                bcrypt.compare(req.body.key2, found.password, function (err, result) {
+                    if (err) {
+                        console.log('something went wrong');
+                    }
+                    if (result) {
+                        return res.status(200).json({ success: true, message: "User login sucessful" });
+                    }
+                    else {
+                        return res.status(401).json({ success: false, message: "Invalid password!" });
+                    }
+
+                });
             }
-            else{
-                return res.status(200).json({success: true, message:"User login sucessful"});
-            }
+
         })
         .catch(err => console.log(err));
 
