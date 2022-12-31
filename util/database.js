19bca9ae4f8152b752130d49d045520e94a8dbc3
rinkoo <rinkookandpal1966@gmail.com>
2022-12-31 11:24:29 +0530

    .

--- util/database.js
+++ util/database.js
@@ -1,4 +1,5 @@
 const Sequelize= require('sequelize');
+require('dotenv').config();
 
 const sequelize= new Sequelize(`${process.env.TableName}`, `${process.env.User_Name}`, `${process.env.Password}`, {
     dialect: 'mysql',
