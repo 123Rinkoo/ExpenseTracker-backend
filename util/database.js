b009c7fa6c60a3c32801bc548b7603dd99444252
rinkoo <rinkookandpal1966@gmail.com>
2023-01-02 14:11:28 +0530

    correction

--- util/database.js
+++ util/database.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const sequelize= new Sequelize(`${process.env.TableName}`, `${process.env.User_Name}`, `${process.env.Password}`, {
     dialect: 'mysql',
-    host: process.env.Host
+    host: `${process.env.Host}`
 });
 
 module.exports=sequelize;
