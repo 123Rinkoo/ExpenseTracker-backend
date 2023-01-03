2faccc498069eba0cd1b779d8840beb11fbc2536
rinkoo <rinkookandpal1966@gmail.com>
2023-01-03 18:40:16 +0530

    final corrections

--- public/updatePassword.js
+++ public/updatePassword.js
@@ -2,7 +2,7 @@ function UpdatePassword(event) {
     event.preventDefault();
     const password = document.getElementById('passy').value;
     document.getElementById('passy').value="";
-    axios.post('http://54.250.168.167:8000/password/updatePassword', { key1: password })
+    axios.post('http://localhost:8000/password/updatePassword', { key1: password })
     .then(result=>console.log(result))
     .catch(err=>console.log(err));
 }
